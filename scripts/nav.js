cfe05d7dc186397578782c9cfa2c302c61be428c
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2023-11-08 14:45:05 -0600

    Created Mobile Nav

---
+++ scripts/nav.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    document.getElementById("mobileHeader").style.display = "none";
+    document.getElementById("mobileMenu").style.height = "100vh";
+    document.getElementById("mobileMenu").style.display = "flex";
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").style.display = "none";
+    document.getElementById("mobileMenu").style.height = "0";
+    document.getElementById("mobileHeader").style.display = "flex";
+}
