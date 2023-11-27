10fc55b2e70f602f01005cf90049e530ab58332e
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2023-11-27 12:22:16 -0600

    Added incorrect answer feedback

--- scripts/checkAnswer.js
+++ scripts/checkAnswer.js
@@ -2,16 +2,18 @@
 // answer - string: answer to the question
 // correctElement - string: id of the element for a correct answer
 // incorrectElement - string: id of the element for an incorrect answer
-function checkAnswer(questionNumber, answer, correctElement) {
+function checkAnswer(questionNumber, answer, correctElement, incorrectElement) {
     questionSet = document.querySelectorAll(`input[name = ${questionNumber}]`);
     
     for (const selected of questionSet) {
         selected.addEventListener('change', () => {
 
             if (selected.value == answer) {
+                document.getElementById(incorrectElement).style.display = "none";
                 document.getElementById(correctElement).style.display = "flex";
             } else {
                 document.getElementById(correctElement).style.display = "none";
+                document.getElementById(incorrectElement).style.display = "flex";
             }
 
         });
